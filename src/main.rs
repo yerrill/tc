353737088367d9a9d371fe7af3ba2afc2301c1e1
Zach Yerrill <26354308+yerrill@users.noreply.github.com>
2025-06-04 00:58:31 -0400

    first tracker request

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,15 @@ use std::fs::File;
 use std::io::prelude::*;
 use tracker::*;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut file = File::open("src\\test.torrent").unwrap();
     let mut contents = Vec::new();
     file.read_to_end(&mut contents).unwrap();
 
     let info = Meta::bdecode(BTypes::bdecode(&contents).unwrap()).unwrap();
 
-    //info.info.pieces = Vec::new();
+    let start = tracker_get(&info, 6881, 0, 0, 0, TrackerEvent::Started).await;
 
-    //println!("{:#?}", info);
-
-    let infohash = info
-        .info_hash()
-        .iter()
-        .map(|b| format!("%{:X}", b))
-        .collect::<String>();
-
-    println!("{}", infohash);
+    let end = tracker_get(&info, 6881, 0, 0, 0, TrackerEvent::Stopped).await;
 }
--- src/tracker.rs
+++ src/tracker.rs
@@ -1,6 +1,9 @@
 use crate::metainfo::*;
+use percent_encoding::{NON_ALPHANUMERIC, percent_encode};
 use rand::{self, Rng};
 use reqwest::get;
+
+// Refactor this into a trait for different tracker protocols
 
 pub struct TrackerDetails<'a> {
     meta: &'a Meta,
@@ -21,28 +24,54 @@ pub enum TrackerEvent {
 }
 
 impl TrackerEvent {
-    fn header_value(&self) -> Option<&'static str> {
+    fn header_value(&self) -> &'static str {
         match self {
-            Self::Started => Some("started"),
-            Self::Completed => Some("completed"),
-            Self::Stopped => Some("stopped"),
-            Self::Empty => None,
+            Self::Started => "started",
+            Self::Completed => "completed",
+            Self::Stopped => "stopped",
+            Self::Empty => "empty",
         }
     }
 }
 
-pub async fn tracker_get(details: &TrackerDetails<'_>) -> Result<(), ()> {
-    let info_hash = details
-        .meta
-        .info_hash()
-        .iter()
-        .map(|b| format!("%{:X}", b))
-        .collect::<String>();
+pub async fn tracker_get(
+    // IP not here rn come back later
+    meta: &Meta,
+    port: u16,
+    uploaded: usize,
+    downloaded: usize,
+    left: usize,
+    event: TrackerEvent,
+) -> Result<(), ()> {
+    let info_hash = percent_encode(meta.info_hash().as_slice(), NON_ALPHANUMERIC).to_string();
 
-    let peer_id = generate_peer_id(); // Generate random ID and return with response
+    println!("{}", info_hash);
 
-    //let body = get()
-    //let query_string = format!("?info_hash{}&peer_id{}", metainfo.info_hash(), peer_id);
+    // Generate random ID and return with response, shouldn't need to be percent encoded with current generation method
+    let peer_id = percent_encode(generate_peer_id().as_bytes(), NON_ALPHANUMERIC).to_string();
+
+    let query_string = format!(
+        "info_hash={}&peer_id={}&port={}&uploaded={}&downloaded={}&left={}&event={}",
+        info_hash,
+        peer_id,
+        port,
+        uploaded,
+        downloaded,
+        left,
+        event.header_value()
+    );
+
+    let response = match reqwest::Client::new()
+        .get(format!("{}?{}", meta.announce, query_string))
+        .send()
+        .await
+    {
+        Ok(r) => r,
+        Err(e) => panic!("{:?}", e),
+    };
+
+    println!("{:?}", response);
+    println!("{:?}", response.text().await);
 
     Ok(())
 }
@@ -56,4 +85,27 @@ pub fn generate_peer_id() -> String {
     }
 
     chars.iter().collect()
+}
+
+// For consistency, percent-encoded octets in the ranges of ALPHA (%41-%5A and %61-%7A),
+// DIGIT (%30-%39), hyphen (%2D), period (%2E), underscore (%5F), or tilde (%7E) should
+// not be created by URI producers and, when found in a URI, should be decoded to their
+// corresponding unreserved characters by URI normalizers.
+fn url_encode(data: &[u8]) -> String {
+    const RESERVED: [char; 18] = [
+        ':', '/', '?', '#', '[', ']', '@', '!', '$', '&', '\'', '(', ')', '*', '+', ',', ';', '=',
+    ];
+
+    let mut output = String::new();
+
+    for ch in data {
+        dbg!(*ch as char);
+        if RESERVED.contains(&(*ch as char)) {
+            output.push_str(format!("%{:02X}", *ch).as_str());
+        } else {
+            output.push(*ch as char);
+        }
+    }
+
+    output
 }
